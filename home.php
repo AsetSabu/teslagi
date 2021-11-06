73e438b40a6a96ec66fa1793f18fbb0860870ae0
AsetSabu <domyputraloborohidpl@gmail.com>
2021-11-06 11:39:40 +0800

    first commit

---
+++ home.php
@@ -0,0 +1,16 @@
+<?php include 'header.php' ?>
+
+<!-- awal content -->
+<div class="section">
+  <div class="container">
+    <div class="content">
+      <h3>Home</h3>
+      <div class="box">
+        <h4>Selamat Datang "<?= $_SESSION['adname'] ?>" di Halaman Utama <a href="" class="grh">Gallery Rai Hawu</a>!</h4>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- akhir content -->
+
+<?php include 'footer.php' ?>
